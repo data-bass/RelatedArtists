cd074a2ab7979cb8854ee677709cbb58b4cc7958
MattRodigheri <mrodigheri@gmail.com>
2018-09-26 16:16:32 -0700

    created & seeded db

---
+++ database/dbtesterCass.js
@@ -0,0 +1,19 @@
+var cassandra = require('cassandra-driver');
+var assert = require('assert')
+
+const client = new cassandra.Client({
+  contactPoints: ['127.0.0.1:9042'],
+  keyspace: 'test'
+});
+
+var results = 1000;
+for (var i = 0; i <= results; i++) {
+  const query = `INSERT INTO test (id, name) VALUES (?, ?)`;
+  const params = [i, i];
+  client.execute(query, params, { prepare: true }, function (err) {
+    assert.ifError(err);
+  });
+  if (i === results) {
+    console.log('inserted ' + i)
+  }
+}
