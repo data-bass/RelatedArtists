abeeb6d0326d22aa3fceee75dbd58e44a5ba0c20
MattRodigheri <mrodigheri@gmail.com>
2018-09-27 12:02:42 -0700

    refactored data generating script to add data in batches

--- database/largeDataGenerator.js
+++ database/largeDataGenerator.js
@@ -6,31 +6,27 @@ var obj = {
    results: []
 };
 
-// let results = [];
-for (let i = 1; i <= 100; i++) {
-  obj.results.push ({
-    id: i,
-    artist_name: faker.name.findName(),
-    listeners: faker.random.number(),
-    artist_image: `https://s3.amazonaws.com/spotifyphotos/${i % 39 + 1}.jpg`,
-    popularSong: faker.lorem.word(),
-  });
-}
-
-// obj.table.push({id: 1, square:2});
-
-var json = JSON.stringify(obj);
-
-fs.writeFile('./artist.json', json, 'utf8', function(err) {
-  if (err) {
+fs.readFile('./artist.json', 'utf8', function readFileCallback(err, data){
+  if (err){
     console.log(err);
   } else {
-    console.log('success');
+    obj = JSON.parse(data);
+    for (let i = 1; i <= 10; i++) {
+      obj.results.push ({
+        id: i,
+        artist_name: faker.name.findName(),
+        listeners: faker.random.number(),
+        artist_image: `https://s3.amazonaws.com/spotifyphotos/${i % 39 + 1}.jpg`,
+        popularSong: faker.lorem.word(),
+      });
+    }
+    json = JSON.stringify(obj);
+    fs.writeFile('./artist.json', json, 'utf8', function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('success');
+      }
+    });
   }
 });
-
-
-
-// for (let j = 0; j < results.length; j++) {
-//   jsonfile.writeFile('loop.json', "id :" + i + " square :" + i*i);
-// }
