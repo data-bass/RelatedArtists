50c8af79a8ea5798bf0d0deb6e9fac26c3cea56d
MattRodigheri <mrodigheri@gmail.com>
2018-10-02 19:54:46 -0700

    refactored data generating code to account for two tables in postgres

--- database/dataInsertSQL.js
+++ database/dataInsertSQL.js
@@ -1,13 +1,13 @@
 var pg = require('pg');
 var fs = require('fs');
 
-var connectionString = "postgres://MattRodigheri:MattRodigheri@localhost/test";
+var connectionString = "postgres://MattRodigheri:MattRodigheri@localhost/artists";
 
 var pgClient = new pg.Client(connectionString);
 
 pgClient.connect();
 
-pgClient.query("copy test from '/Users/MattRodigheri/artist0.csv' delimiter ',' csv header;", (err, res) => {
+pgClient.query("copy related_artists from '/Users/MattRodigheri/related_artist9.csv' delimiter ',' csv header;", (err, res) => {
     if (err) {
       console.log(err);
     } else {
--- database/largeDataGenerator.js
+++ database/largeDataGenerator.js
@@ -1,27 +1,40 @@
 const faker = require ('faker');
 const fs = require('fs');
 
-var string = 'id,artist_name,listeners,artist_image,popularSong,related_artists\n';
+// var string = 'id|artist_name|listeners|artist_image|popularSong|related_artists\n';
+// var string = 'id,artist_name,listeners,artist_image,popularSong,related_artists\n';
+// var string = 'id,artist_name,listeners,image,song\n';
+var string = 'id,artist_id,related_artist_id\n';
+let k = 0;
+let randomId;
+let j = 0;
 
-for (let i = 1; i <= 1000000; i++) {
-  let insertCount = 1;
-  let uniqueIdArr = [];
-  let k = 0;
-  let randomId;
-  while (k < 10) {
-    randomId = Math.floor (Math.random () * Math.floor (1000000));
+for (let i = 90000001; i <= 100000000; i++) {
+  // let uniqueIdArr = [];
+  if (k % 10 === 0) {
+    j++;
+  }
+  // while (k < 10) {
+    randomId = Math.floor (Math.random () * Math.floor (10000000));
     if (randomId === i) {
       continue;
     } else {
       k++;
-      uniqueIdArr.push(`""${randomId}""`);
+      // uniqueIdArr.push(`${randomId}`);
+      // uniqueIdArr.push(`""${randomId}""`);
     }
-  }
-  var csvArr = '"[' + uniqueIdArr + ']"'
-  string += `${i},${faker.name.findName()},${faker.random.number()},https://loremflickr.com/320/240/alien?lock=${randomId},${faker.lorem.word()},${csvArr}\n`
+  // }
+  // var csvArr = '"[' + uniqueIdArr + ']"';
+  // var csvArr = '"' + uniqueIdArr.join(',') + '"';
+
+  string += `${i},${j},${randomId}\n`
+  //string += `${i},${faker.name.findName()},${faker.random.number()},https://loremflickr.com/320/240/alien?lock=${randomId},${faker.lorem.word()}\n`
+  // string += `${i},${faker.name.findName()},${faker.random.number()},https://loremflickr.com/320/240/alien?lock=${randomId},${faker.lorem.word()},${csvArr}\n`
+  // string += `${i}|${faker.name.findName()}|${faker.random.number()}|https://loremflickr.com/320/240/alien?lock=${randomId}|${faker.lorem.word()}|${csvArr}\n`
+
 }
 
-fs.writeFile('./../../artist0.csv', string, 'utf8', function(err) {
+fs.writeFile('./../../related_artist9.csv', string, 'utf8', function(err) {
   if (err) {
     console.log(err);
   } else {
