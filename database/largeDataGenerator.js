824dbe1d4b49ea67958df5c18b2e80a913e1ec8b
MattRodigheri <mrodigheri@gmail.com>
2018-10-03 22:21:05 -0700

    fixed largeDataGenerator variables & random number generator

--- database/largeDataGenerator.js
+++ database/largeDataGenerator.js
@@ -1,36 +1,36 @@
 const faker = require ('faker');
 const fs = require('fs');
 
-// var string = 'id|artist_name|listeners|artist_image|popularSong|related_artists\n';
-// var string = 'id,artist_name,listeners,artist_image,popularSong,related_artists\n';
-// var string = 'id,artist_name,listeners,image,song\n';
-var string = 'id,artist_id,related_artist_id\n';
-let k = 0;
+// var string = 'id|artist_name|listeners|artist_image|popularSong|related_artists\n'; //cassandra
+// var string = 'id,artist_name,listeners,artist_image,popularSong,related_artists\n'; //postgres one table
+// var string = 'id,artist_name,listeners,image,song\n'; //postgres artists table
+var string = 'id,artist_id,related_artist_id\n'; //postgres related_artists table
+// let k = 0;
 let randomId;
-let j = 0;
+let j = 9000000;
 
 for (let i = 90000001; i <= 100000000; i++) {
   // let uniqueIdArr = [];
-  if (k % 10 === 0) {
+  if (i % 10 === 0) {
     j++;
   }
+  randomId = 1 + Math.floor(Math.random() * 10000000);
   // while (k < 10) {
-    randomId = Math.floor (Math.random () * Math.floor (10000000));
-    if (randomId === i) {
-      continue;
-    } else {
-      k++;
-      // uniqueIdArr.push(`${randomId}`);
-      // uniqueIdArr.push(`""${randomId}""`);
-    }
+  //   if (randomId === i) {
+  //     continue;
+  //   } else {
+  //     k++;
+  //     // uniqueIdArr.push(`${randomId}`);
+  //     // uniqueIdArr.push(`""${randomId}""`);
+  //   }
   // }
   // var csvArr = '"[' + uniqueIdArr + ']"';
   // var csvArr = '"' + uniqueIdArr.join(',') + '"';
 
-  string += `${i},${j},${randomId}\n`
-  //string += `${i},${faker.name.findName()},${faker.random.number()},https://loremflickr.com/320/240/alien?lock=${randomId},${faker.lorem.word()}\n`
-  // string += `${i},${faker.name.findName()},${faker.random.number()},https://loremflickr.com/320/240/alien?lock=${randomId},${faker.lorem.word()},${csvArr}\n`
-  // string += `${i}|${faker.name.findName()}|${faker.random.number()}|https://loremflickr.com/320/240/alien?lock=${randomId}|${faker.lorem.word()}|${csvArr}\n`
+  string += `${i},${j},${randomId}\n` //postgres related_artists table
+  // string += `${i},${faker.name.findName()},${faker.random.number()},https://loremflickr.com/320/240/alien?lock=${randomId},${faker.lorem.word()}\n`  //postgres artists table
+  // string += `${i},${faker.name.findName()},${faker.random.number()},https://loremflickr.com/320/240/alien?lock=${randomId},${faker.lorem.word()},${csvArr}\n` //postgres one table
+  // string += `${i}|${faker.name.findName()}|${faker.random.number()}|https://loremflickr.com/320/240/alien?lock=${randomId}|${faker.lorem.word()}|${csvArr}\n` //cassandra
 
 }
 
