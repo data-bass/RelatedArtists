909bee5b4a5f8c5d3c7fa999c817cb74b967b279
MattRodigheri <mrodigheri@gmail.com>
2018-10-11 17:27:24 -0700

    added redis

--- server/index.js
+++ server/index.js
@@ -4,17 +4,27 @@ const app = express();
 const db = require('../database/index.js');
 const path = require('path');
 const cors = require('cors');
+const redis = require('redis');
+const client = redis.createClient();
+
 
 app.use (cors ());
 app.use (express.static (path.join (__dirname + '/../public')));
 
 app.get(`/artist/:id/relatedArtists`, (req, res) => {
-  db.getRelatedArtists (req.params.id, (error, data) => {
-    if (error) {
-      res.status(503).send(error);
+  client.get(req.params.id, (err, data) => {
+    if (data) {
+      res.send(data);
     } else {
-      res.send (data.rows);
-    }
+      db.getRelatedArtists (req.params.id, (error, data) => {
+        if (error) {
+          res.status(503).send(error);
+        } else {
+          client.setex(req.params.id, 3600, JSON.stringify(data.rows))
+          res.send (data.rows);
+        }
+      });
+     }
   });
 });
 
